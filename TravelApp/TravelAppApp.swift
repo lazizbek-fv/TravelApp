1dbd540cd2c0e343c47970ccc8004fc20295f164
Lazizbek <mywave.17@gmail.com>
2024-12-12 09:12:09 +0100

    Initial Commit

---
+++ TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Lazizbek Fayzullayev on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
